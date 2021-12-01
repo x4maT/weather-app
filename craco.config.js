1ad15269e1f857cbae3ea58656a2103c0e36515f
Maksym Sudakov <msudakov.work@gmail.com>
2021-12-01 14:57:28 +0200

    add tailwind css adn craco

---
+++ craco.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  style: {
+    postcss: {
+      // eslint-disable-next-line global-require,import/no-extraneous-dependencies
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+};
