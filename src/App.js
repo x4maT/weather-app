68e270e38c41ab792cf018cb152d8e03f2b50f81
Maksym Sudakov <msudakov.work@gmail.com>
2021-12-07 11:06:15 +0200

    add layout components to App, add initial geoLocation features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { IntlProvider } from 'react-intl';
 
 import './App.css';
 import Header from './components/layout/Header';
+import Main from './components/layout/Main';
+import Footer from './components/layout/Footer';
+import { ErrorPortal } from './components/layout/ErrorPortal';
 import translations from './i18n/translations';
 import { ENGLISH } from './i18n/locales';
+import { getByGeoLocation } from './store/weather/actions';
+import { setError, removeError } from './store/common/slice';
+import { normalizeLocale } from './utils/locales';
+
+const getCurrentGeoLocation = (successCallback, errorCallback) =>
+  navigator.geolocation &&
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
 const App = () => {
-  const { locale } = useSelector((state) => state.common);
+  const dispatch = useDispatch();
+  const { locale } = useSelector(({ common }) => common, shallowEqual);
+  const { currentLocationId } = useSelector(
+    ({ weather }) => weather,
+    shallowEqual,
+  );
+
+  const successGeoLocationRequest = (pos) => {
+    const { latitude: lat, longitude: lon } = pos.coords;
+
+    dispatch(
+      getByGeoLocation({
+        current: true,
+        lat,
+        lon,
+        lang: normalizeLocale(locale),
+      }),
+    );
+  };
+
+  const failGeoLocationRequest = (err) => {
+    dispatch(setError(err));
+  };
+
+  useEffect(() => {
+    dispatch(removeError());
+  }, []);
+
+  useEffect(() => {
+    if (!currentLocationId) {
+      getCurrentGeoLocation(successGeoLocationRequest, failGeoLocationRequest);
+    }
+  }, [currentLocationId]);
 
   return (
     <IntlProvider
@@ -16,8 +58,11 @@ const App = () => {
       locale={locale}
       defaultLocale={ENGLISH}
     >
-      <div className="App">
+      <div className="App flex flex-col h-screen">
         <Header currentLocale={locale} />
+        <Main />
+        <Footer />
+        <ErrorPortal />
       </div>
     </IntlProvider>
   );
