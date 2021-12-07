e11ae7e42a811e720c3b1278756a2ae2684227b5
Maksym Sudakov <msudakov.work@gmail.com>
2021-12-07 11:09:43 +0200

    add create/delete reducer features and switchMeasurementUnitType to weather slice

--- src/store/weather/slice.js
+++ src/store/weather/slice.js
@@ -3,7 +3,9 @@ import { createSlice } from '@reduxjs/toolkit';
 export const weatherSlice = createSlice({
   name: 'weather',
   initialState: {
-    loading: 'idle',
+    error: null,
+    currentLocationId: null,
+    isCelsius: true,
     cities: [],
     favorites: [],
   },
@@ -11,9 +13,40 @@ export const weatherSlice = createSlice({
     setLoading: (state, { payload }) => {
       state.isLoading = payload;
     },
+    setCity: (state, { payload }) => {
+      if (payload.current) {
+        state.currentLocationId = payload.sys.id;
+      }
+
+      const index = state.cities.findIndex(
+        (city) => city.sys.id === payload.sys.id,
+      );
+
+      if (~index) {
+        state.cities[index] = payload;
+        if (state.currentLocationId === payload.sys.id) {
+          state.cities[index].current = true;
+        }
+      } else {
+        state.cities.push(payload);
+      }
+
+      state.cities = [...state.cities];
+    },
+    deleteCityById: (state, { payload }) => {
+      state.cities = state.cities.filter((city) => city.sys.id !== payload.id);
+    },
+    switchMeasurementUnitType: (state) => {
+      state.isCelsius = !state.isCelsius;
+    },
   },
 });
 
-export const { setLoading, setError } = weatherSlice.actions;
+export const {
+  setLoading,
+  setCity,
+  switchMeasurementUnitType,
+  deleteCityById,
+} = weatherSlice.actions;
 
 export default weatherSlice.reducer;
